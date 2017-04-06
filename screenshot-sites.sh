81667d40f27497d8080b6e9ff49e0d7e45ba1094
aah13002 <aaron.huang@uconn.edu>
2017-04-06 05:43:23 -0400

    Check for no command line arguments in bash script

--- screenshot-sites.sh
+++ screenshot-sites.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+    echo "No command line input found."
+    echo "Enter the file name of a text file containing websites to screenshot."
+    exit
+fi    
+
 FILE_NAME="$1"
 
 echo "Analyzing file:" $FILE_NAME
